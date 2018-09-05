096cf8803949142020373dcc3004c38fa6e72c68
Lucas Lacquaniti <lucasmlacquaniti@gmail.com>
2018-09-05 17:46:42 -0300

    Primer Commit del TP1

---
+++ src/ar/edu/unlam/pb2/TrabajoPractico1/Triangulo.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.pb2.TrabajoPractico1;
+
+public class Triangulo {
+
+	public Triangulo(Double lado1, Double lado2, Double lado3) {
+
+	}
+
+	public Double calcularPerimetro(Double lado1, Double lado2, Double lado3) {
+
+		Double perimetro = 0.0;
+
+		perimetro = lado1 + lado2 + lado3;
+
+		return perimetro;
+	}
+
+	public Double calcularAltura(Double altura, Double base) {
+
+		Double area = 0.0;
+
+		area = (altura * base)/ 2;
+
+		return area;
+	}
+
+}
